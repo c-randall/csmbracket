9f870506db9ac9930ecbf37b0ba3d1cc301628d0
Corey Randall <randa@MacBook-CRR.local>
2022-11-20 20:53:17 -0700

    first deploy attempt

--- csmbracket/settings.py
+++ csmbracket/settings.py
@@ -132,8 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [ BASE_DIR / 'bracket/static' ]
+
+STATIC_ROOT = '/var/www/csmbracket/static'
+
+MEDIA_URL = 'images/'
+MEDIA_ROOT = BASE_DIR / 'bracket/static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
